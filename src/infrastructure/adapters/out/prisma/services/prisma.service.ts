732d02d30dca142f5506291d1f03d3dd68adb421
Ruan Failache <ruanfailache@gmail.com>
2025-12-01 23:34:26 -0300

    chore: configure swagger

--- src/infrastructure/adapters/out/prisma/services/prisma.service.ts
+++ src/infrastructure/adapters/out/prisma/services/prisma.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 import { PrismaClient } from '../generated/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
     constructor(configService: ConfigService) {
         super({
-            adapter: new PrismaPg({
-                url: configService.get('DATABASE_URL')
-            })
+            adapter: new PrismaPg(
+                new Pool({
+                    connectionString: configService.get('DATABASE_URL')
+                })
+            )
         });
     }
 }
