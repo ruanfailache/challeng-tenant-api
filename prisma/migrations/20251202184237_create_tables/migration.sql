9c2a66c184cebff018c2e6e17351b234b7b8b7a1
Ruan Failache <ruanfailache@gmail.com>
2025-12-02 15:42:55 -0300

    fix: migrations

--- prisma/migrations/20251202184237_create_tables/migration.sql
+++ prisma/migrations/20251202184237_create_tables/migration.sql
@@ -6,7 +6,7 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "name" TEXT,
+    "name" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
