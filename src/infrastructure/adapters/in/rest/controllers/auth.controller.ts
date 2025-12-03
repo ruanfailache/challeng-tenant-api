a4c645ee34db18465d4ac3dc47d5a9187b290c13
Ruan Failache <ruanfailache@gmail.com>
2025-12-03 10:15:35 -0300

    feat: add swagger to auth controller

--- src/infrastructure/adapters/in/rest/controllers/auth.controller.ts
+++ src/infrastructure/adapters/in/rest/controllers/auth.controller.ts
@@ -1,14 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common'
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { CreateUserUseCase } from '@/application/usecases/user/create-user.usecase'
 import { Public } from '@/infrastructure/security/decorators/public.decorator'
 import { CreateUserRequest } from '../dto/requests/user/create-user.request'
 
 @Controller('auth')
+@ApiTags('Authentication')
 export class AuthController {
   constructor(private readonly createUserUseCase: CreateUserUseCase) {}
 
   @Public()
   @Post('/signup')
+  @ApiOperation({ summary: 'User Sign Up', description: 'Register a new user' })
+  @ApiResponse({ status: 201, description: 'User successfully registered' })
+  @ApiResponse({ status: 400, description: 'Invalid request data' })
+  @ApiResponse({ status: 409, description: 'User already exists' })
   signUp(@Body() createUserRequest: CreateUserRequest) {
     return this.createUserUseCase.execute(createUserRequest)
   }
