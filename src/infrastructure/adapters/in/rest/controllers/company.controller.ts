6bb3f8dedd52c476bf4a6494032199710f46ff98
Ruan Failache <ruanfailache@gmail.com>
2025-12-03 10:29:54 -0300

    feat: create endpoint to list all companies paginated

--- src/infrastructure/adapters/in/rest/controllers/company.controller.ts
+++ src/infrastructure/adapters/in/rest/controllers/company.controller.ts
@@ -1,7 +1,9 @@
 import {
   Body,
   Controller,
+  Get,
   Post,
+  Query,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common'
@@ -13,14 +15,37 @@ import {
   ApiTags,
 } from '@nestjs/swagger'
 import { CreateCompanyUseCase } from '@/application/usecases/company/create-company.usecase'
+import { ListUserCompaniesUseCase } from '@/application/usecases/company/list-user-companies.usecase'
 import { CurrentUser } from '@/infrastructure/security/decorators/current-user.decorator'
 import { type LoggedUserPayload } from '@/infrastructure/security/dto/payloads/logged-user.payload'
+import { PageRequest } from '../dto/requests/common/pagination.request'
 import { CreateCompanyRequest } from '../dto/requests/company/create-company.request'
 
 @Controller('company')
 @ApiTags('Companies')
 export class CompanyController {
-  constructor(private readonly createCompanyUseCase: CreateCompanyUseCase) {}
+  constructor(
+    private readonly createCompanyUseCase: CreateCompanyUseCase,
+    private readonly listUserCompaniesUseCase: ListUserCompaniesUseCase,
+  ) {}
+
+  @Get()
+  @ApiOperation({
+    summary: 'List User Companies',
+    description: 'List all companies the logged user has membership',
+  })
+  @ApiResponse({ status: 200, description: 'Companies retrieved successfully' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  list(
+    @Query() pageRequest: PageRequest,
+    @CurrentUser() user: LoggedUserPayload,
+  ) {
+    return this.listUserCompaniesUseCase.execute(
+      user.userId,
+      pageRequest.page,
+      pageRequest.size,
+    )
+  }
 
   @Post()
   @UseInterceptors(FileInterceptor('logo'))
